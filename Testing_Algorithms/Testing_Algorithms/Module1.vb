845e417e39c4b8a59771ead3657883b883ec4e56
Fairooz-cmd <60594513+Fairooz-cmd@users.noreply.github.com>
2020-02-03 22:14:29 +0600

    Add files via upload

---
+++ Testing_Algorithms/Testing_Algorithms/Module1.vb
@@ -0,0 +1,21 @@
+﻿Module Module1
+    Sub Main()
+
+        Handle_2s_Complement()
+
+    End Sub
+
+    Sub Handle_2s_Complement()
+
+        Dim byte1 As SByte = -128
+
+        Console.WriteLine(SByte.MaxValue)
+        Console.WriteLine(SByte.MinValue)
+        Console.WriteLine()
+
+        byte1 -= 8
+        Console.WriteLine(byte1)
+
+        Console.ReadKey()
+    End Sub
+End Module
